812ec887bd8ad359c104527c9b6c605fee5ec171
Magnus Hoff <maghoff@gmail.com>
2016-03-23 11:16:39 +0100

    Add comparisons to native numbers and direct use of rationals in example

--- example.js
+++ example.js
@@ -4,15 +4,19 @@ var rationals = require('rationals');
 var ir = require('infix-rationals');
 
 
-var x = rationals(0), y = 0;
+var x = rationals(0), y = rationals(0), z = 0;
 
 for (var i = 0; i < 10; ++i) {
 	x = ir.memCompile("$0 + 0.1")(x);
-	y = y + 0.1;
+	y = y.add(rationals(1, 10));
+	z = z + 0.1;
+
+	console.log(x.display() + "  " + x.val() + "  " + z);
 }
 
 assert(x === rationals(1));
-assert(y !== 1);
+assert(y === rationals(1));
+assert(z !== 1);
 
 
 // Rewritten example from rationals:
